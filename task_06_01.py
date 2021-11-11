f73d948efa6f6a4c57af73bef15894e569b1dd38
EvgeniiSapozhkov <sapozhkovevgenii@gmail.com>
2021-11-11 16:05:24 +0300

    Add file "task_06_1"

---
+++ task_06_01.py
@@ -0,0 +1,14 @@
+from random import randint
+
+n = 3
+m = 4
+arr = []
+
+for i in range(n):
+    arr1 = []
+    for j in range(m):
+        p = randint(1, 9)
+        arr1.append(p)
+    arr.append(arr1)
+
+print(arr)
