29070f016badf2c1b4f2e23683cbd62075c7200d
EvgeniiSapozhkov <sapozhkovevgenii@gmail.com>
2021-11-11 16:15:43 +0300

    Add file "task_06_3"

---
+++ task_06_03.py
@@ -0,0 +1,19 @@
+from random import randint
+
+n = 3
+m = 4
+arr = []
+
+for i in range(n):
+    arr1 = []
+    for j in range(m):
+        p = randint(1, 9)
+        arr1.append(p)
+    arr.append(arr1)
+
+sum_elem = 0
+for i in arr:
+    for j in i:
+        sum_elem += j
+
+print(sum_elem)
