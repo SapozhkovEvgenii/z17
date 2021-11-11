e58f592f391d3518a0aab363fe2dc657a9ce5de3
EvgeniiSapozhkov <sapozhkovevgenii@gmail.com>
2021-11-11 16:20:16 +0300

    Add file "task_06_6"

---
+++ task_06_06.py
@@ -0,0 +1,27 @@
+from random import randint
+
+n = 3
+m = 4
+arr = []
+
+for i in range(n):
+    arr1 = []
+    for j in range(m):
+        p = randint(1, 9)
+        arr1.append(p)
+    arr.append(arr1)
+print(arr)
+my_list_index = []
+
+for i in range(m):
+    summa_elem = 0
+    for j in range(n):
+        summa_elem += arr[j][i]
+    my_list_index.append(summa_elem)
+print(my_list_index)
+index_max_column = my_list_index.index(max(my_list_index))
+print(index_max_column)
+
+
+
+
